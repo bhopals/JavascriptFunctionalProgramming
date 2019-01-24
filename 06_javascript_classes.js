339b4fe4261720444d0e50b1e7aa12f2b2d42ce3
Bhopal Singh <bhopalsinghsis@gmail.com>
2019-01-24 22:02:16 +0400

    Java Script Es6 Class

--- 06_javascript_classes.js
+++ 06_javascript_classes.js
@@ -2,3 +2,9 @@
 Java Script Classes
 ***/
 
+ - Class Declaration 
+ - Class extends
+ - Static and Instance Methods of a Class
+ - Hoisting Concept and how it is different for Class and Function Declaration
+ - Protoype and how proto chain works
+
